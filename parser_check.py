29972a92329b6b7a4a78f8f8261f0a6dbddf7da9
veterAN911 <ya.tty2014@yandex.ru>
2024-10-08 23:36:04 +0300

    develop парсинг позиции

--- parser_check.py
+++ parser_check.py
@@ -3,6 +3,11 @@ import json
 def pars_check():
     with open('check.json', 'r', encoding='utf-8') as file:
         data = json.load(file)  # Загружаем JSON данные из файла
+    print(data['ticket']['items'][0]['options'])
+    print(f"Колличество: {data['ticket']['items'][0]['options']['quantity']}")
+    print(f"Наименование: {data['ticket']['items'][0]['options']['name']}")
+    print(f"Сумма позиции: {data['ticket']['items'][0]['options']['price']}")
+    print(f"Сумма NDS позиции: {data['ticket']['items'][0]['options']['ndsSum']}")
     print("\nСумма чека ", data['ticket']['totalSum'])
     print("\nДата чека ", data['ticket']['transactionDate'])
     for payment in data['ticket']['payments']:
