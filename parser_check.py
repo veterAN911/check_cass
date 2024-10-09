f1689f09fef78fa38f742237f395d1dbae067dc2
veteran <ya.tty2014@yandex.ru>
2024-10-09 12:49:41 +0300

    develop промежуточное локальное сохранение

--- parser_check.py
+++ parser_check.py
@@ -3,9 +3,15 @@ import json
 def pars_check():
     with open('check.json', 'r', encoding='utf-8') as file:
         data = json.load(file)  # Загружаем JSON данные из файла
-    print("\nСумма чека ", data['ticket']['totalSum'])
     print("\nДата чека ", data['ticket']['transactionDate'])
+    print("\nФискальный номер ", data['ticket']['fiscalDocumentNumber'])
+    print("\nNDS чека 10% ", data['ticket']['nds10'])
+    print("\nNDS чека 20% ", data['ticket']['nds20'])
     for payment in data['ticket']['payments']:
         if payment['sum'] != 0.0:
             print(f"Сумма: {payment['sum']}, Тип оплаты: {payment['paymentType']}")
+    num_position = 0
+    for position in data['ticket']['items']:
+        num_position += 1
+        print(f"Позиция №{num_position}: {position['options']['name']}")
 pars_check()
